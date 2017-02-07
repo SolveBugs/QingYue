febdba188637df5a1fd517b4ad1acbb7887945db
甄强 <qiang.zhen@dnurse.cn>
2017-02-07 16:47:04 +0800

    增加侧滑菜单

--- app/src/main/java/com/chsj/qingyue/MainActivity.java
+++ app/src/main/java/com/chsj/qingyue/MainActivity.java
@@ -1,9 +1,13 @@
 package com.chsj.qingyue;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.View;
 
 import com.chsj.qingyue.adapter.MyFragmentPagerAdapter;
 import com.chsj.qingyue.base.BaseActivity;
@@ -17,18 +21,39 @@ public class MainActivity extends BaseActivity {
     private TabLayout mTabLayout;
     private MyFragmentPagerAdapter myFragmentPagerAdapter;
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideBack(true);
         initViews();
+        initDrawer();
         isNetWorkAvalable = NetWorkUtils.isConnect(this);
         if (!isNetWorkAvalable) {//当前没有网络，进入 网络设置界面
             //TODO 进行网络设置
             Intent intent = new Intent(MainActivity.this, SettingNetActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void initDrawer() {
+        //创建返回键，并实现打开关/闭监听
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     private void initViews() {
@@ -41,6 +66,7 @@ public class MainActivity extends BaseActivity {
         mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
         mTabLayout.setupWithViewPager(mViewPager);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
     }
 
     @Override
